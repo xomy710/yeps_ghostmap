52d72dabd2a47934913bdfa82d2c046a9e896993
JJEONG <48850536+xomy710@users.noreply.github.com>
2020-12-29 22:36:07 +0900

    main_
    _oo_

---
+++ Assets/YJ/Script/Cam_Rotate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Cam_Rotate : MonoBehaviour
+{
+
+    public float move = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void left()
+    {
+
+
+        this.transform.Rotate(0, -move, 0);
+
+
+    }
+
+    public void right()
+    {
+        this.transform.Rotate(0, move, 0);
+
+    }
+
+    public void up()
+    {
+        this.transform.Rotate(-move, 0, 0);
+
+    }
+
+    public void down()
+    {
+        this.transform.Rotate(move, 0, 0);
+
+    }
+
+    public void Scene_opening()
+    {
+        SceneManager.LoadScene("MAP");
+    }
+}
---
+++ Assets/YJ/Script/NextScene.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Scene_1()
+    {
+        SceneManager.LoadScene("1_1");
+    }
+
+    public void Scene_2()
+    {
+        SceneManager.LoadScene("1_2");
+    }
+
+
+    public void Scene_3()
+    {
+        SceneManager.LoadScene("1_3");
+    }
+
+    public void Scene_4()
+    {
+        SceneManager.LoadScene("1_4");
+    }
+
+    public void Scene_5()
+    {
+        SceneManager.LoadScene("1_5");
+    }
+
+    public void Scene_6()
+    {
+        SceneManager.LoadScene("1_6");
+    }
+
+    public void Scene_7()
+    {
+        SceneManager.LoadScene("1_7");
+    }
+
+    public void Scene_9()
+    {
+        SceneManager.LoadScene("1_9");
+    }
+
+    public void Scene_10()
+    {
+        SceneManager.LoadScene("1_10");
+    }
+
+    public void Scene_8()
+    {
+        SceneManager.LoadScene("1_8");
+    }
+
+    public void Scene_opening()
+    {
+        SceneManager.LoadScene("MAP");
+    }
+}
